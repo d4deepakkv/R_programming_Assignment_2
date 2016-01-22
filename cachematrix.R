94667535d6ad9c282153e02282430f17d34cb982
d4deepakkv <d4deepakkv@gmail.com>
2016-01-22 16:13:19 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  The two functions below are used to create a special object that stores a matrix 
+##  and caches its inverse.
 
-## Write a short comment describing this function
+## mmakeCacheMatrix creates a special "vector", which is really a list containing a function to
+
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse of matrix
+## get the value of the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matinv <- NULL
+  set <- function(y) {
+    x <<- y
+    matinv <<- NULL
+  }
+  get <- function() x
+  setmatinv <- function(inv) matinv <<- inv
+  getmatinv <- function() matinv
+  list(set = set, get = get,
+       setmatinv = setmatinv,
+       getmatinv = getmatinv)
+  
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "cache matrix" 
+## created with the above function. However, it first checks to see 
+## if the inverse matrix has already been calculated. If so, it gets the inverse 
+## from the cache and skips the computation. Otherwise, it calculates 
+## the inverse of the matrix and sets the value of the inverse in the cache 
+## via the setmatinv function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The argument x to the cachesolve function should be the list returned  by the 
+## makecachematrix function.
+
+cacheSolve <- function(x,...) {
+  matinv <- x$getmatinv()
+  if(!is.null(matinv)) {
+    message("getting cached data")
+    return(matinv)
+  }
+  data <- x$get()
+  matinv <- solve(data, ...)
+  x$setmatinv(matinv)
+  matinv
 }
